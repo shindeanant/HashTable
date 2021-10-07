a32b3da8c12967372d937ef87904164e7f5e1fda
shindeanant <shindeanant1997@gmail.com>
2021-10-07 15:46:23 +0530

    ADD: gitignore file added

---
+++ main/java/com/bridgelabzz/HashTable/App.java
@@ -0,0 +1,9 @@
+package com.bridgelabzz.HashTable;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Hash Table program" );
+    }
+}
